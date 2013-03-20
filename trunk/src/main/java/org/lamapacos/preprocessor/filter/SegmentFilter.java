f709bf374333b9978607d411b134b2cca0974da2
707609789@qq.com <707609789@qq.com@774e6db0-174b-25d2-82b4-6bc88ba4fbe9>
2013-03-20 17:31:27 +0000

    [LAMAPACOS][COMMON][ORG.LAMAPACOS.PREPROCESSOR.FILTER] this is a  pages filter,  it use lamapacos-preprocessor.xml:directly.accept.url.regex to define url filter rules:   for examples: +.* accept all                           -.*  accept nothing nothing   rules are checked from left to right,and return whichever matches
      run configuration:
                                  -dump <segmentHome> <outputDir>

--- trunk/src/main/java/org/lamapacos/preprocessor/filter/SegmentFilter.java
+++ trunk/src/main/java/org/lamapacos/preprocessor/filter/SegmentFilter.java
@@ -16,32 +16,20 @@
  */
 package org.lamapacos.preprocessor.filter;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.io.PrintStream;
-import java.io.PrintWriter;
-import java.io.Writer;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.hadoop.conf.Configurable;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.MapFile;
-import org.apache.hadoop.io.SequenceFile;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.io.WritableComparable;
@@ -49,7 +37,6 @@ import org.apache.hadoop.mapred.FileInputFormat;
 import org.apache.hadoop.mapred.FileOutputFormat;
 import org.apache.hadoop.mapred.JobClient;
 import org.apache.hadoop.mapred.JobConf;
-import org.apache.hadoop.mapred.MapFileOutputFormat;
 import org.apache.hadoop.mapred.MapReduceBase;
 import org.apache.hadoop.mapred.Mapper;
 import org.apache.hadoop.mapred.OutputCollector;
@@ -57,27 +44,78 @@ import org.apache.hadoop.mapred.RecordWriter;
 import org.apache.hadoop.mapred.Reducer;
 import org.apache.hadoop.mapred.Reporter;
 import org.apache.hadoop.mapred.SequenceFileInputFormat;
-import org.apache.hadoop.mapred.SequenceFileOutputFormat;
 import org.apache.hadoop.util.Progressable;
-import org.apache.nutch.crawl.CrawlDatum;
 import org.apache.nutch.crawl.NutchWritable;
-import org.apache.nutch.parse.ParseData;
-import org.apache.nutch.parse.ParseText;
 import org.apache.nutch.protocol.Content;
-import org.apache.nutch.util.HadoopFSUtil;
 import org.apache.nutch.util.NutchConfiguration;
 import org.apache.nutch.util.NutchJob;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /** Dump the content of a segment. */
 public class SegmentFilter extends Configured implements Reducer<Text, NutchWritable, Text, Text> {
 
 	public static final Logger LOG = LoggerFactory.getLogger(SegmentFilter.class);
 
-	long recNo = 0L;
-
-	private boolean co;//, fe, ge, pa, pd, pt;
 	private FileSystem fs;
+	private static final String URL_REGEX = "directly.accept.url.regex";
+	private static final String URL_REGEX_SEP = ";";
+	
+	public static interface Filter extends Configurable{
+		public abstract boolean accept(Object obj);
+	}
+	
+	public static abstract class FilterBase implements Filter {
+		Configuration conf;
+		public Configuration getConf() {
+			return this.conf;
+		}
+	}
+	
+	public static  class RegexsFilter extends FilterBase {
+		static class PolarPattern {
+			char polar;  //‘+’ or '-', which means accept this pattern or not
+			Pattern pattern;
+			PolarPattern(char polar, Pattern p) {
+				this.polar = polar;
+				this.pattern = p;
+			}
+		}
+		
+		static List<PolarPattern> patterns = new ArrayList<PolarPattern>();
+		
+		public RegexsFilter() {}
+		public static void setPatterns(Configuration conf) {
+			String regexs = conf.get(URL_REGEX, "+.*");
+			String [] splits = regexs.split(URL_REGEX_SEP);
+			for(String regex : splits) {
+				try{
+					PolarPattern p = new PolarPattern(regex.charAt(0),Pattern.compile(regex.substring(1)));
+					patterns.add(p);
+				} catch(PatternSyntaxException e) {
+					throw new IllegalArgumentException("Invalid pattern: "+regex);
+				}
+			}
+		}
+		@Override
+		public boolean accept(Object obj) {
+			for(PolarPattern p : patterns) {
+				if(p.polar == '-' && p.pattern.matcher(obj.toString()).matches()) {
+					return false;
+				} else if(p.polar == '+' && p.pattern.matcher(obj.toString()).matches()) {
+					return true;
+				}
+			}
+			return false;
+		}
 
+		@Override
+		public void setConf(Configuration conf) {
+			this.conf = conf;
+		}
+		
+	}
+	
 	public static class InputCompatMapper extends MapReduceBase implements Mapper<WritableComparable, Writable, Text, NutchWritable> {
 		private Text newKey = new Text();
 
@@ -124,9 +162,10 @@ public class SegmentFilter extends Configured implements Reducer<Text, NutchWrit
 		super(null);
 	}
 
-	public SegmentFilter(Configuration conf, boolean co, boolean fe, boolean ge, boolean pa, boolean pd, boolean pt) {
+	public SegmentFilter(Configuration conf) {
 		super(conf);
-		this.co = co;
+		conf.addResource("lamapacos-preprocessor.xml");
+		RegexsFilter.setPatterns(conf);
 		try {
 			this.fs = FileSystem.get(getConf());
 		} catch (IOException e) {
@@ -134,10 +173,13 @@ public class SegmentFilter extends Configured implements Reducer<Text, NutchWrit
 		}
 	}
 
+	private RegexsFilter regexsFilter = new RegexsFilter();
 	@Override
 	public void configure(JobConf job) {
 		setConf(job);
-		this.co = getConf().getBoolean("segment.reader.co", true);
+		//conf file
+		job.addResource("lamapacos-preprocessor.xml");
+		RegexsFilter.setPatterns(job);
 		try {
 			this.fs = FileSystem.get(getConf());
 		} catch (IOException e) {
@@ -147,7 +189,6 @@ public class SegmentFilter extends Configured implements Reducer<Text, NutchWrit
 
 	private JobConf createJobConf() {
 		JobConf job = new NutchJob(getConf());
-		job.setBoolean("segment.reader.co", this.co);
 		return job;
 	}
 
@@ -158,19 +199,12 @@ public class SegmentFilter extends Configured implements Reducer<Text, NutchWrit
 	@Override
 	public void reduce(Text key, Iterator<NutchWritable> values, OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
 		StringBuffer dump = new StringBuffer();
-
-		dump.append("\nRecno:: ").append(recNo++).append("\n");
-		dump.append("URL:: " + key.toString() + "\n");
+		if(!regexsFilter.accept(key.toString())) return;
+		
 		while (values.hasNext()) {
-			Writable value = values.next().get(); // unwrap
-			if (value instanceof CrawlDatum) {
-				dump.append("\nCrawlDatum::\n").append(((CrawlDatum) value).toString());
-			} else if (value instanceof Content) {
-				dump.append("\nContent::\n").append(((Content) value).toString());
-			} else if (value instanceof ParseData) {
-				dump.append("\nParseData::\n").append(((ParseData) value).toString());
-			} else if (value instanceof ParseText) {
-				dump.append("\nParseText::\n").append(((ParseText) value).toString());
+			Writable value = values.next().get(); // unwrap	
+			if (value instanceof Content) {
+				dump.append(((Content) value).toString());
 			} else if (LOG.isWarnEnabled()) {
 				LOG.warn("Unrecognized type: " + value.getClass());
 			}
@@ -178,87 +212,44 @@ public class SegmentFilter extends Configured implements Reducer<Text, NutchWrit
 		output.collect(key, new Text(dump.toString()));
 	}
 
-	public void dump(Path segment, Path output) throws IOException {
+	public void dump(Path segmentHome, Path output) throws IOException {
 
 		if (LOG.isInfoEnabled()) {
-			LOG.info("SegmentReader: dump segment: " + segment);
+			LOG.info("SegmentFilter: filtering segment: " + segmentHome);
 		}
 
 		JobConf job = createJobConf();
-		job.setJobName("read " + segment);
-
-		if (co) FileInputFormat.addInputPath(job, new Path(segment, Content.DIR_NAME));
-
+		job.setJobName("filter " + segmentHome);
+		
+		FileStatus[] paths = fs.listStatus(segmentHome);
+		for(FileStatus path : paths) {
+			if(path.isDir()) {
+				FileInputFormat.addInputPath(job, new Path(segmentHome,new Path(path.getPath(), Content.DIR_NAME)));
+			}
+		}
+		
+		
 		job.setInputFormat(SequenceFileInputFormat.class);
 		job.setMapperClass(InputCompatMapper.class);
 		job.setReducerClass(SegmentFilter.class);
 
-		Path tempDir = new Path(job.get("hadoop.tmp.dir", "/tmp") + "/segread-" + new java.util.Random().nextInt());
-		fs.delete(tempDir, true);
+//		Path tempDir = new Path(job.get("hadoop.tmp.dir", "/tmp") + "/segfilter-" + new java.util.Random().nextInt());
+//		fs.delete(tempDir, true);
 
-		FileOutputFormat.setOutputPath(job, tempDir);
+		FileOutputFormat.setOutputPath(job, output);
 		job.setOutputFormat(TextOutputFormat.class);
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(NutchWritable.class);
 
 		JobClient.runJob(job);
 
-		// concatenate the output
-		Path dumpFile = new Path(output, job.get("segment.dump.dir", "dump"));
-
-		// remove the old file
-		fs.delete(dumpFile, true);
-		FileStatus[] fstats = fs.listStatus(tempDir, HadoopFSUtil.getPassAllFilter());
-		Path[] files = HadoopFSUtil.getPaths(fstats);
-
-		PrintWriter writer = null;
-		int currentRecordNumber = 0;
-		if (files.length > 0) {
-			writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(fs.create(dumpFile))));
-			try {
-				for (int i = 0; i < files.length; i++) {
-					Path partFile = files[i];
-					try {
-						currentRecordNumber = append(fs, job, partFile, writer, currentRecordNumber);
-					} catch (IOException exception) {
-						if (LOG.isWarnEnabled()) {
-							LOG.warn("Couldn't copy the content of " + partFile.toString() + " into " + dumpFile.toString());
-							LOG.warn(exception.getMessage());
-						}
-					}
-				}
-			} finally {
-				writer.close();
-			}
-		}
-		fs.delete(tempDir, true);
 		if (LOG.isInfoEnabled()) {
-			LOG.info("SegmentReader: done");
+			LOG.info("SegmentFilter: done");
 		}
 	}
 
-	/** Appends two files and updates the Recno counter */
-	private int append(FileSystem fs, Configuration conf, Path src, PrintWriter writer, int currentRecordNumber) throws IOException {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(fs.open(src)));
-		try {
-			String line = reader.readLine();
-			while (line != null) {
-				if (line.startsWith("Recno:: ")) {
-					line = "Recno:: " + currentRecordNumber++;
-				}
-				writer.println(line);
-				line = reader.readLine();
-			}
-			return currentRecordNumber;
-		} finally {
-			reader.close();
-		}
-	}
 
 	private static final int MODE_DUMP = 0;
-
-//	private static final int MODE_LIST = 1;
-
 	private static final int MODE_DEFAULT = -0x3ffff;
 
 	public static void main(String[] args) throws Exception {
@@ -268,39 +259,15 @@ public class SegmentFilter extends Configured implements Reducer<Text, NutchWrit
 		}
 		int mode = MODE_DEFAULT;
 		if (args[0].equals("-dump")) mode = MODE_DUMP;
-//		else if (args[0].equals("-list")) mode = MODE_LIST;
-		
-		boolean co = true;
-		boolean fe = true;
-		boolean ge = true;
-		boolean pa = true;
-		boolean pd = true;
-		boolean pt = true;
-		// collect general options
-		for (int i = 1; i < args.length; i++) {
-			if (args[i].equals("-nocontent")) {
-				co = false;
-				args[i] = null;
-			} else if (args[i].equals("-nofetch")) {
-				fe = false;
-				args[i] = null;
-			} else if (args[i].equals("-nogenerate")) {
-				ge = false;
-				args[i] = null;
-			} else if (args[i].equals("-noparse")) {
-				pa = false;
-				args[i] = null;
-			} else if (args[i].equals("-noparsedata")) {
-				pd = false;
-				args[i] = null;
-			} else if (args[i].equals("-noparsetext")) {
-				pt = false;
-				args[i] = null;
-			}
+		else {
+			System.err.println("Missing operation argument:(-dump... | ...)");
+			usage();
+			return;
 		}
+		
 		Configuration conf = NutchConfiguration.create();
 		final FileSystem fs = FileSystem.get(conf);
-		SegmentFilter segmentReader = new SegmentFilter(conf, co, fe, ge, pa, pd, pt);
+		SegmentFilter segmentReader = new SegmentFilter(conf);
 		// collect required args
 		switch (mode) {
 		case MODE_DUMP:
@@ -326,30 +293,14 @@ public class SegmentFilter extends Configured implements Reducer<Text, NutchWrit
 	}
 
 	private static void usage() {
-		System.err.println("Usage: SegmentReader (-dump ... | -list ... | -get ...) [general options]\n");
+		System.err.println("Usage: SegmentFilter (-dump ... | ...) [general options]\n");
 		System.err.println("* General options:");
-		System.err.println("\t-nocontent\tignore content directory");
-		System.err.println("\t-nofetch\tignore crawl_fetch directory");
-		System.err.println("\t-nogenerate\tignore crawl_generate directory");
-		System.err.println("\t-noparse\tignore crawl_parse directory");
-		System.err.println("\t-noparsedata\tignore parse_data directory");
-		System.err.println("\t-noparsetext\tignore parse_text directory");
 		System.err.println();
 		System.err.println("* SegmentReader -dump <segment_dir> <output> [general options]");
 		System.err.println("  Dumps content of a <segment_dir> as a text file to <output>.\n");
-		System.err.println("\t<segment_dir>\tname of the segment directory.");
+		System.err.println("\t<segment_dir>\tname of the segment home.");
 		System.err.println("\t<output>\tname of the (non-existent) output directory.");
 		System.err.println();
-		System.err.println("* SegmentReader -list (<segment_dir1> ... | -dir <segments>) [general options]");
-		System.err.println("  List a synopsis of segments in specified directories, or all segments in");
-		System.err.println("  a directory <segments>, and print it on System.out\n");
-		System.err.println("\t<segment_dir1> ...\tlist of segment directories to process");
-		System.err.println("\t-dir <segments>\t\tdirectory that contains multiple segments");
-		System.err.println();
-		System.err.println("* SegmentReader -get <segment_dir> <keyValue> [general options]");
-		System.err.println("  Get a specified record from a segment, and print it on System.out.\n");
-		System.err.println("\t<segment_dir>\tname of the segment directory.");
-		System.err.println("\t<keyValue>\tvalue of the key (url).");
 		System.err.println("\t\tNote: put double-quotes around strings with spaces.");
 	}
 }
