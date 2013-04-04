72bfce6a1de2bce28e75ac2ca26e3f3551181255
1074005082@qq.com <1074005082@qq.com@774e6db0-174b-25d2-82b4-6bc88ba4fbe9>
2013-04-04 16:42:28 +0000

    [LAMAPACOS][COMMON][ORG.LAMAPACOS.IO] add TwoTuple, it has two fields, the first and the second. Any writable class that has two fields can extend TwoTuple.

---
+++ trunk/src/main/java/org/lamapacos/io/TwoTuple.java
@@ -0,0 +1,67 @@
+package org.lamapacos.io;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+import org.apache.hadoop.io.Writable;
+
+public class TwoTuple<S extends Writable,T extends Writable> implements Writable{
+	private S first;
+	private T second;
+
+	@Override
+	public int hashCode() {
+		int ret = 31;
+		ret += ret * 7 + first.hashCode();
+		ret += ret * 7 + second.hashCode();
+		return ret;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(!(obj instanceof TwoTuple))
+			return false;
+		TwoTuple tmp = (TwoTuple)obj;
+		return this.first.equals(tmp.getFirst()) ? this.second.equals(tmp.getSecond()) : false;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("[first:");
+		builder.append(first.toString());
+		builder.append(",second:");
+		builder.append(second.toString());
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
+	@Override
+	public void readFields(DataInput in) throws IOException {
+		first.readFields(in);
+		second.readFields(in);
+		
+	}
+
+	@Override
+	public void write(DataOutput out) throws IOException {
+		first.write(out);
+		second.write(out);
+	}
+	
+	public Writable getFirst() {
+		return this.first;
+	}
+	public void setScore(S first) {
+		this.first = first;
+	}
+	public Writable getSecond() {
+		return second;
+	}
+	public void setSecond(T second) {
+		this.second = second;
+	}
+	
+}
