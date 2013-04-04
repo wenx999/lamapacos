c1b77914610fae6dfb42c5e097447f84d1ee7043
707609789@qq.com <707609789@qq.com@774e6db0-174b-25d2-82b4-6bc88ba4fbe9>
2013-04-04 02:33:54 +0000

    [LAMAPACOS][TEST][ORG.LAMAPACOS.PREPROCESSOR.EXTRACTION.Tokenizer] get NLPIRTokenizer out of Tokenizer

--- trunk/src/main/java/org/lamapacos/preprocessor/extraction/Tokenizer.java
+++ trunk/src/main/java/org/lamapacos/preprocessor/extraction/Tokenizer.java
@@ -3,8 +3,6 @@
  */
 package org.lamapacos.preprocessor.extraction;
 
-import kevin.zhang.NLPIR;
-
 
 /**
  * @author hadoop
@@ -14,29 +12,3 @@ public interface Tokenizer {
 	public String segment(String source);
 }
 
-class NLPIRTokenizer implements Tokenizer{
-	public NLPIRTokenizer() {}
-	
-	public String segment(String source) {
-		try {
-			NLPIR testNLPIR = new NLPIR();
-
-			String argu = "";
-			System.out.println("NLPIR_Init");
-			if (testNLPIR.NLPIR_Init(argu.getBytes("UTF-8"), 1) == false) {
-				System.out.println("Init Fail!");
-				return null;
-			}
-			// 导入用户词典前
-			byte nativeBytes[] = testNLPIR.NLPIR_ParagraphProcess(source.getBytes("UTF-8"), 1);
-			String nativeStr = new String(nativeBytes, 0, nativeBytes.length, "UTF-8");
-
-			System.out.println("分词结果为： " + nativeStr);
-			testNLPIR.NLPIR_Exit();
-			return new String(nativeBytes);
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-		return null;
-	}
-}
