b29f550dd65f52afb5790c418cc3be616097ed03
1074005082@qq.com <1074005082@qq.com@774e6db0-174b-25d2-82b4-6bc88ba4fbe9>
2013-04-09 17:12:39 +0000

    [LAMAPACOS][COMMON][ORG.LAMAPACOS.TRAININGSET] Fix bug.The original code may return a wrong tag.

--- trunk/src/main/java/org/lamapacos/trainingset/SumOfWeightTagger.java
+++ trunk/src/main/java/org/lamapacos/trainingset/SumOfWeightTagger.java
@@ -20,6 +20,7 @@ import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
@@ -45,6 +46,7 @@ public class SumOfWeightTagger extends Mapper<WritableComparable, LamapacosWrita
 	public static final char WORD_CHARACTERISTIC = '/';
 	public static final int DEGREE_SCOPE = 8;
 	public static final String PUNCTUATION_MARK_LABEL = "w";
+	public static final String DEFAULT_SENTIMENT = "CO";
 	
 	private Configuration conf;
 	
@@ -94,88 +96,94 @@ public class SumOfWeightTagger extends Mapper<WritableComparable, LamapacosWrita
 	private final List<TaggedWord> wordList = new LinkedList();
 	@Override
 	public  TaggedContent tag(LamapacosWritable source) {
-		Writable tmptuple = source.get();
-		if(tmptuple instanceof SegmentedContentTuple) {
-			SegmentedContentTuple tuple = (SegmentedContentTuple)tmptuple;
-			Text content = tuple.getContent();
-			String segmentedContent = tuple.getSegmentedContent().toString();
-			String[] words = segmentedContent.split(WORD_SEP);
-			TreeMap<String,Integer> intensitySum = new TreeMap<String, Integer>();
-			for(String word : words) {
-				String []wordAndCh = StringUtils.split(word, ' ', WORD_CHARACTERISTIC);
-				if(wordAndCh.length != 2) {
-					try {
+		try{
+			Writable tmptuple = source.get();
+			if(tmptuple instanceof SegmentedContentTuple) {
+				SegmentedContentTuple tuple = (SegmentedContentTuple)tmptuple;
+				Text content = tuple.getContent();
+				String segmentedContent = tuple.getSegmentedContent().toString().substring(12).trim();
+				String[] words = segmentedContent.split(WORD_SEP);
+				TreeMap<String,Integer> intensitySum = new TreeMap<String, Integer>();
+				for(String word : words) {
+					String []wordAndCh = StringUtils.split(word, ' ', WORD_CHARACTERISTIC);
+					if(wordAndCh.length == 0 || wordAndCh[0] == "")
+						continue;
+					if(wordAndCh.length != 2) {
 						throw new Exception("Corrupted Word!" + Arrays.toString(wordAndCh));
-					} catch (Exception e) {
-						e.printStackTrace();
 					}
+					TaggedWord taggedWord = new TaggedWord();
+					taggedWord.setWord(new Text(wordAndCh[0]));
+					taggedWord.setCharacteristic(new Text(wordAndCh[1]));
+					String[] sentimentAndIntensity = dictionary.get(wordAndCh[0]);
+					if(sentimentAndIntensity != null) {
+						taggedWord.setSentiment(new Text(sentimentAndIntensity[0]));
+						taggedWord.setIntensity(new IntWritable(Integer.valueOf(sentimentAndIntensity[1])));
+					}
+					wordList.add(taggedWord);
 				}
-				TaggedWord taggedWord = new TaggedWord();
-				taggedWord.setWord(new Text(wordAndCh[0]));
-				taggedWord.setCharacteristic(new Text(wordAndCh[1]));
-				String[] sentimentAndIntensity = dictionary.get(wordAndCh[0]);
-				if(sentimentAndIntensity != null) {
-					taggedWord.setSentiment(new Text(sentimentAndIntensity[0]));
-					taggedWord.setIntensity(new IntWritable(Integer.valueOf(sentimentAndIntensity[1])));
-				}
-				wordList.add(taggedWord);
-			}
-			
-			//deal with degree word
-			for(int i = 0; i < wordList.size(); i++) {
-				TaggedWord taggedWord  = wordList.get(i);
-				String[] degree = degreeDictionary.get(taggedWord.getWord());
-				if(degree != null) {  //a degree word
-					try {
-						if(taggedWord.getCharacteristic().toString().startsWith(PUNCTUATION_MARK_LABEL)) {
-							throw new Exception("Word in degree Dictionary can't be punctuation mark:" + taggedWord.getWord().toString());
-						}
-						
-						Integer degreeValue = Integer.valueOf(degree[0]);
-						if(degreeValue < 0) {
+				
+				//deal with degree word
+				for(int i = 0; i < wordList.size(); i++) {
+					TaggedWord taggedWord  = wordList.get(i);
+					String[] degree = degreeDictionary.get(taggedWord.getWord().toString());
+					if(degree != null) {  //a degree word
+						try {
+							if(taggedWord.getCharacteristic().toString().startsWith(PUNCTUATION_MARK_LABEL)) {
+								throw new Exception("Word in degree Dictionary can't be punctuation mark:" + taggedWord.getWord().toString());
+							}
 							
-						} else if(degreeValue > 0) {
-							for(int j = i + 1; j < wordList.size() 
-								&& !wordList.get(j).getCharacteristic().toString().startsWith(PUNCTUATION_MARK_LABEL); j++) {
-								IntWritable sentimentIntensity = wordList.get(j).getIntensity();
-								if(sentimentIntensity != null) {  //this word is a sentiment word
-									wordList.get(j).getIntensity().set(sentimentIntensity.get() * degreeValue);
+							Float degreeValue = Float.valueOf(degree[0]);
+							if(degreeValue < 0) {
+								
+							} else if(degreeValue > 0) {
+								for(int j = i + 1; j < wordList.size() 
+									&& !wordList.get(j).getCharacteristic().toString().startsWith(PUNCTUATION_MARK_LABEL); j++) {
+									IntWritable sentimentIntensity = wordList.get(j).getIntensity();
+									if(sentimentIntensity != null) {  //this word is a sentiment word
+										wordList.get(j).getIntensity().set((int)(sentimentIntensity.get() * degreeValue));
+									}
 								}
 							}
+						} catch(NumberFormatException nfe) {
+							throw new NumberFormatException("Degree of word:" + taggedWord.getWord() + " is corrupted!");
+						} catch (Exception e) {
+							e.printStackTrace();
 						}
-					} catch(NumberFormatException nfe) {
-						throw new NumberFormatException("Degree of word:" + taggedWord.getWord() + " is corrupted!");
-					} catch (Exception e) {
-						e.printStackTrace();
+					} else if (taggedWord.getSentiment() != null) { // a sentiment word
+						String sentiment = taggedWord.getSentiment().toString();
+						Integer sum = null;
+						if(intensitySum.containsKey(sentiment)) {
+							sum = intensitySum.get(sentiment);
+						} else {
+							sum = new Integer(0);
+						}
+						sum += taggedWord.getIntensity().get();
+						intensitySum.put(sentiment, sum);
 					}
-				} else if (taggedWord.getSentiment() != null) { // a sentiment word
-					String sentiment = taggedWord.getSentiment().toString();
-					Integer sum = null;
-					if(intensitySum.containsKey(sentiment)) {
-						sum = intensitySum.get(sentiment);
-					} else {
-						sum = new Integer(0);
+				}
+				Integer maxFreq = Integer.MIN_VALUE;
+				String maxSentiment = null;
+				for(Entry<String, Integer> entry : intensitySum.entrySet()) {
+					if(entry.getValue() > maxFreq) {
+						maxFreq = entry.getValue();
+						maxSentiment = entry.getKey();
 					}
-					sum += taggedWord.getIntensity().get();
 				}
-			}
-			Integer maxFreq = Integer.MIN_VALUE;
-			String maxSentiment = null;
-			for(Entry<String, Integer> entry : intensitySum.entrySet()) {
-				if(entry.getValue() > maxFreq) {
-					maxFreq = entry.getValue();
-					maxSentiment = entry.getKey();
+				if(maxSentiment != null) {
+					return new TaggedContent(new Text(content), new Text(maxSentiment));
 				}
-			}
-			return new TaggedContent(content, new Text(maxSentiment));
-		}else {
-			try {
+				else {
+					return new TaggedContent(new Text(content), new Text(DEFAULT_SENTIMENT));
+				}
+			}else {
 				throw new Exception("Wrong type" + tmptuple.getClass().getCanonicalName());
-			} catch (Exception e) {
-				e.printStackTrace();
 			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			wordList.clear();
 		}
-		
 		return null;
 	}
 	
@@ -194,7 +202,7 @@ public class SumOfWeightTagger extends Mapper<WritableComparable, LamapacosWrita
 		
 		Configuration conf = getConf() == null ? new Configuration():getConf();
 		Job job = new Job(conf, "tag " + sourceHome);
-		job.setInputFormatClass(TextInputFormat.class);
+		job.setInputFormatClass(SequenceFileInputFormat.class);
 		job.setOutputFormatClass(TextOutputFormat.class);
 		FileInputFormat.setInputPaths(job, sourceHome);
 		FileOutputFormat.setOutputPath(job, output);
