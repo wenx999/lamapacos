e9021ae14b58d6e14a13ed6a7841e05f1d20f97b
1074005082@qq.com <1074005082@qq.com@774e6db0-174b-25d2-82b4-6bc88ba4fbe9>
2013-04-12 02:33:14 +0000

    [LAMAPACOS][COMMON][ORG.LAMAPACOS.TRAININGSET] This change is to pull away the making a tag to the record from the SumOfWeirhtTagger to the SimpleTagger class which implements the interface of Tagger.java. And the interface of Tagger.java has added two method to support choice the feather of the record and parsing the record. So now the method of tag() is only to choice a fit sentiment tag and tag this record. In addition, the SimpleTagger class now support to deal with the negative words. The TagMapping.xml is the mapping of the opposite sentiment tag. And it also include father tag map to childeren tag.

--- trunk/src/main/java/org/lamapacos/trainingset/SumOfWeightTagger.java
+++ trunk/src/main/java/org/lamapacos/trainingset/SumOfWeightTagger.java
@@ -1,20 +1,12 @@
 package org.lamapacos.trainingset;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.TreeMap;
 
-import jxl.read.biff.BiffException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.mapreduce.Job;
@@ -23,12 +15,8 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
-import org.apache.hadoop.util.StringUtils;
 import org.lamapacos.io.LamapacosWritable;
-import org.lamapacos.io.SegmentedContentTuple;
 import org.lamapacos.io.TaggedContent;
-import org.lamapacos.io.TaggedWord;
-import org.lamapacos.util.DictionaryUtil;
 import org.mortbay.log.Log;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -39,7 +27,8 @@ import org.slf4j.LoggerFactory;
  * @author hadoop
  *
  */
-public class SumOfWeightTagger extends Mapper<WritableComparable, LamapacosWritable, WritableComparable, Writable> implements Tagger{
+@SuppressWarnings("rawtypes")
+public class SumOfWeightTagger extends Mapper<WritableComparable, LamapacosWritable, WritableComparable, Writable> {
 	public static final Logger LOG = LoggerFactory.getLogger(SumOfWeightTagger.class);
 	public static final String WORD_SEP = " ";
 	public static final char WORD_CHARACTERISTIC = '/';
@@ -64,18 +53,11 @@ public class SumOfWeightTagger extends Mapper<WritableComparable, LamapacosWrita
 		return this.conf;
 	}
 	
-	private static Map<String, String[]> dictionary;
-	private static Map<String, String[]> degreeDictionary;
+
+	private static SimpleTagger simpleTag;
 	@Override
 	public void setup(Context context) {
-		dictionary = new DictionaryUtil(context.getConfiguration()).readDictionary();
-		try {
-			degreeDictionary = new DictionaryUtil().readDegreeDictionary();
-		} catch (BiffException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		simpleTag = new SimpleTagger(context.getConfiguration());
 	}
 	
 	/**
@@ -87,103 +69,8 @@ public class SumOfWeightTagger extends Mapper<WritableComparable, LamapacosWrita
 	@Override
 	public void map(WritableComparable key, LamapacosWritable value, Context context) 
 		throws IOException, InterruptedException {
-		TaggedContent taggedContent = tag(value);
+		TaggedContent taggedContent = simpleTag.tag(value);
 		context.write(key, taggedContent);
-	}
-	
-	
-	private final List<TaggedWord> wordList = new LinkedList();
-	@Override
-	public  TaggedContent tag(LamapacosWritable source) {
-		try{
-			Writable tmptuple = source.get();
-			if(tmptuple instanceof SegmentedContentTuple) {
-				SegmentedContentTuple tuple = (SegmentedContentTuple)tmptuple;
-				Text content = tuple.getContent();
-				String segmentedContent = tuple.getSegmentedContent().toString().substring(12).trim();
-				String[] words = segmentedContent.split(WORD_SEP);
-				TreeMap<String,Integer> intensitySum = new TreeMap<String, Integer>();
-				for(String word : words) {
-					String []wordAndCh = StringUtils.split(word, ' ', WORD_CHARACTERISTIC);
-					if(wordAndCh.length == 0 || wordAndCh[0] == "")
-						continue;
-					if(wordAndCh.length != 2) {
-						throw new Exception("Corrupted Word!" + Arrays.toString(wordAndCh));
-					}
-					TaggedWord taggedWord = new TaggedWord();
-					taggedWord.setWord(new Text(wordAndCh[0]));
-					taggedWord.setCharacteristic(new Text(wordAndCh[1]));
-					String[] sentimentAndIntensity = dictionary.get(wordAndCh[0]);
-					if(sentimentAndIntensity != null) {
-						taggedWord.setSentiment(new Text(sentimentAndIntensity[0]));
-						taggedWord.setIntensity(new IntWritable(Integer.valueOf(sentimentAndIntensity[1])));
-					}
-					wordList.add(taggedWord);
-				}
-				
-				//deal with degree word
-				for(int i = 0; i < wordList.size(); i++) {
-					TaggedWord taggedWord  = wordList.get(i);
-					String[] degree = degreeDictionary.get(taggedWord.getWord().toString());
-					if(degree != null) {  //a degree word
-						try {
-							if(taggedWord.getCharacteristic().toString().startsWith(PUNCTUATION_MARK_LABEL)) {
-								throw new Exception("Word in degree Dictionary can't be punctuation mark:" + taggedWord.getWord().toString());
-							}
-							
-							Float degreeValue = Float.valueOf(degree[0]);
-							if(degreeValue < 0) {
-								
-							} else if(degreeValue > 0) {
-								for(int j = i + 1; j < wordList.size() 
-									&& !wordList.get(j).getCharacteristic().toString().startsWith(PUNCTUATION_MARK_LABEL); j++) {
-									IntWritable sentimentIntensity = wordList.get(j).getIntensity();
-									if(sentimentIntensity != null) {  //this word is a sentiment word
-										wordList.get(j).getIntensity().set((int)(sentimentIntensity.get() * degreeValue));
-									}
-								}
-							}
-						} catch(NumberFormatException nfe) {
-							throw new NumberFormatException("Degree of word:" + taggedWord.getWord() + " is corrupted!");
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-					} else if (taggedWord.getSentiment() != null) { // a sentiment word
-						String sentiment = taggedWord.getSentiment().toString();
-						Integer sum = null;
-						if(intensitySum.containsKey(sentiment)) {
-							sum = intensitySum.get(sentiment);
-						} else {
-							sum = new Integer(0);
-						}
-						sum += taggedWord.getIntensity().get();
-						intensitySum.put(sentiment, sum);
-					}
-				}
-				Integer maxFreq = Integer.MIN_VALUE;
-				String maxSentiment = null;
-				for(Entry<String, Integer> entry : intensitySum.entrySet()) {
-					if(entry.getValue() > maxFreq) {
-						maxFreq = entry.getValue();
-						maxSentiment = entry.getKey();
-					}
-				}
-				if(maxSentiment != null) {
-					return new TaggedContent(new Text(content), new Text(maxSentiment));
-				}
-				else {
-					return new TaggedContent(new Text(content), new Text(DEFAULT_SENTIMENT));
-				}
-			}else {
-				throw new Exception("Wrong type" + tmptuple.getClass().getCanonicalName());
-			}
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}finally {
-			wordList.clear();
-		}
-		return null;
 	}
 	
 	/**
--- trunk/src/main/java/org/lamapacos/util/Constant.java
+++ trunk/src/main/java/org/lamapacos/util/Constant.java
@@ -17,8 +17,17 @@ public class Constant {
 	public static final String DEGREE_DICTIONARY_REGEX = "degree.word.dictionary.location";
 	public static final String DEFAULT_DEGEDIC_VALUE = "resource/degreeDictionary.xls";
 	
+	public static final String TAGMAP_DICTIONARY_REGEX = "tagmapping.dictionary.location";
+	public static final String DEFAULT_TAGMAPDIC_VALUE = "resource/TagMapping.xls";
+	
 	public static final String EXTRACT_DATASTRU_REGEX= "extract.datastructure";
 	public static final String DEFAULT_EXTDATASTR_VALUE = "org.hadood.io.Text";
 	
 	public static final String CONTENT_SEP = "__CONT_SEP";
+	
+//	public static final String WORD_SEP = " ";
+//	public static final char WORD_CHARACTERISTIC = '/';
+//	public static final int DEGREE_SCOPE = 8;
+//	public static final String PUNCTUATION_MARK_LABEL = "w";
+//	public static final String DEFAULT_SENTIMENT = "CO";
 }
